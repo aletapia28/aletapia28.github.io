89a54936d39ed80f1a1f93d137e3937144024d62
aletapia28 <aletapiab28@gmail.com>
2019-12-15 21:05:36 -0600

    validaciones

--- ControlUsuarios/login.js
+++ ControlUsuarios/login.js
@@ -1,43 +1,76 @@
-function test_pass() {
-    var res;
-    var str = document.getElementById("password").value;
-    if (str.match(/[a-z]/g) && str.match(
-        /[A-Z]/g) && str.match(
-            /[0-9]/g) && str.length >= 8) {
-        res = true;
+
+function test_pass(pass) {
+    if (pass.match(/[a-z]/g) && pass.match(
+        /[A-Z]/g) && pass.match(
+            /[0-9]/g) && pass.length >= 8) {
+        return true;
     }
-    else
-        res = false;
-        
+    else {
+        alert("La contraseña no es válida")
+        return (false)
+    }
+
+}
+
+function ValidateEmail(mail) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+        return (true)
+    }
+    alert("La dirección de correo no es válida")
+    return (false)
+}
 
 
-    document.getElementById("correo").value = res;
-    return res;
 
-} 
+function IniciarSesion() {
+    var passw = document.getElementById("password").value;
+    var mail = document.getElementById("correo").value;
 
-
-function validate_pass (){
     
-    window.location.href = "registrarse.html";
-
+    if (test_pass(passw) == true && ValidateEmail(mail))
+        window.location.href = "index.html"
+    
 }
 
 function ShowPass() {
     var x = document.getElementById("password");
     if (x.type === "password") {
-      x.type = "text";
+        x.type = "text";
     } else {
-      x.type = "password";
+        x.type = "password";
     }
-  }
+}
 
-function registrarse(){
-    window.location.href= "login.html"
+function registrarse() {
+    var nombre = document.getElementById("Nombre").value;
+    var passw = document.getElementById("password").value;
+
+    if (test_pass(passw) == true)
+        window.location.href = "login.html"
+    else
+        alert("La contraseña no es valida");
 }
 
 
-function cambiar_pass(){
-    window.location.href= "login.html"
+function cambiar_pass() {
+    var passw = document.getElementById("passwordNueva").value;
+    var passwNueva = document.getElementById("password2").value;
+    var mail = document.getElementById("email").value;
 
+    if (test_pass(passw) == true && passwNueva===passw && ValidateEmail(mail)==true)
+        
+            window.location.href = "index.html"
+        
+        
+    else
+        alert("La contraseña no es valida");
+
+
+}
+
+function recover() {
+    var mail = document.getElementById("email").value;
+
+    if(ValidateEmail(mail)==true)
+        window.location.href = "login.html"
 }
